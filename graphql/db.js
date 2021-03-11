2e38d753a8e87fb2e0608708efc72f97f8f9c5a8
SYONG <ghdgus24@naver.com>
2021-03-11 14:33:22 +0900

    graphql 기본적인 add, delete

---
+++ graphql/db.js
@@ -0,0 +1,49 @@
+let movies = [
+  {
+    id: 0,
+    name: "Star Wars - The New One",
+    score: 1,
+  },
+  {
+    id: 1,
+    name: "Avengers - Infinity War",
+    score: 8,
+  },
+  {
+    id: 2,
+    name: "The Godfather I",
+    score: 99,
+  },
+  {
+    id: 3,
+    name: "Logan",
+    score: 7,
+  },
+];
+
+export const getMovies = () => movies;
+
+export const getById = (id) => {
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,13 @@
-const syong = {
-  name: "syongg",
-  age: 30,
-  gender: "male",
-};
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
   Query: {
-    person: () => syong,
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
